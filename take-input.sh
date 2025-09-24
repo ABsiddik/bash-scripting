ae215422eee3a1c3963aeecb8ea2cbe49f879f24
absiddik <absiddika@gmail.com>
2025-09-24 20:55:44 +0600

    Take input from user

---
+++ take-input.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Enter your name: " name
+read -p "Enter your phone: " phone
+read -p "Enter your email: " email
+read -p "Enter your age: " age
+
+echo "Here is your information below"
+echo "Name: $name"
+echo "Phone: $phone"
+echo "Email: $email"
+echo "Age: $age"
